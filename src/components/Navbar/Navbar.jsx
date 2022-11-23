715345fc1a34651a4d947188ed260b16df032f73
Felipe Calvo <fcalvob@udd.cl>
2022-11-23 20:17:14 -0300

    avance

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return
+    <nav className="navbar">
+        <h1>Trakk</h1>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+    </nav>
+}
+
+export default Navbar;
