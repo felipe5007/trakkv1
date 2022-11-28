85cfd8b8ef5d1ae4a6c425868ec78dd59af3f1b7
Felipe Calvo <fcalvob@udd.cl>
2022-11-27 21:35:09 -0300

    Pre entrega 1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Topnav from './components/Navbar/Topnav';
+import ItemListContainer from './components/Cart/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <Topnav/>
     </div>
+     <div className="App">
+     <ItemListContainer/>
+   </div>
   );
 }
 
